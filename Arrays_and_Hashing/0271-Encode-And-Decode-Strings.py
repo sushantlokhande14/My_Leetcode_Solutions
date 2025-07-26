00d36b89a4ee46812b939b7ebcdd16c551ab3e2e
Sushant <sushantlokhande094@gmail.com>
2025-07-25 23:47:17 -0700

    initial full submission with decode function added

--- Arrays_and_Hashing/0271-Encode-And-Decode-Strings.py
+++ Arrays_and_Hashing/0271-Encode-And-Decode-Strings.py
@@ -1,5 +1,3 @@
-
-
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
@@ -9,4 +7,27 @@ class Solution:
             res+= str(len(s))+ "#" + s 
         
         return res
+
+    def decode(self, s: str) -> List[str]:
+
+        res = []
+
+        i = 0 
+
+        while i < len(s): 
+            j = i 
+
+            if s[j] != "#": 
+                j+=1 
+
+            length = int(s[i:j])
+
+            i = j +1 
+            j = i + length
+
+            res.append(s[i:j])
+
+            i = j 
+        return res 
+
 
