992e52938fcd80c303897cb44109783913862533
Sushant <sushantlokhande094@gmail.com>
2025-07-26 17:54:28 -0700

    Solved error in line 32

--- Arrays_and_Hashing/00238-Product-Of-Array-Except-Self.py
+++ Arrays_and_Hashing/00238-Product-Of-Array-Except-Self.py
@@ -29,8 +29,7 @@ class Solution:
         suf[n-1]= 1
 
         for i in range( 1, n): 
-            pre[i] = nums[i-+
-            1]*pre[i-1 ]
+            pre[i] = nums[i-1]*pre[i-1 ]
 
         for i in range(n-2, -1,-1):
             suf[i] = nums[i+1] * suf[i+1]
