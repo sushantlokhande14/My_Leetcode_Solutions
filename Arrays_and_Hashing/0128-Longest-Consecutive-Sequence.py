68a9a6cf274a3d8d76b30b00688fcf3c9b54decb
Sushant <sushantlokhande094@gmail.com>
2025-07-28 22:15:00 -0700

    added complete optimal solution

--- Arrays_and_Hashing/0128-Longest-Consecutive-Sequence.py
+++ Arrays_and_Hashing/0128-Longest-Consecutive-Sequence.py
@@ -20,3 +20,22 @@ class Solution:
     
 
 # Hashset - O(n)
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        res = 0 
+        store = set(nums)
+
+        for num in nums: 
+
+            if (num-1) not in store : 
+
+                length = 0 
+
+                while (num+length) in store: 
+                    length += 1 
+
+                res = max(res, length)
+
+        return res
