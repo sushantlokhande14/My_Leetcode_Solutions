44cc11819ac071888eb325a78c7ff97de7236c3e
Sushant <sushantlokhande094@gmail.com>
2025-07-29 23:28:01 -0700

    added outline structure for linked_list design

---
+++ Extras/designing_a_linked_list/linked_list.py
@@ -0,0 +1,19 @@
+class LinkedList:
+    
+    def __init__(self):
+
+    
+    def get(self, index: int) -> int:
+        
+
+    def insertHead(self, val: int) -> None:
+        
+
+    def insertTail(self, val: int) -> None:
+        
+
+    def remove(self, index: int) -> bool:
+        
+
+    def getValues(self) -> List[int]:
+        
