defd6ef596080a1fe8fdadf3c40c0578a5b0f82b
Sushant <sushantlokhande094@gmail.com>
2025-07-29 23:50:29 -0700

    final structure

--- Extras/designing_a_linked_list/linked_list.py
+++ Extras/designing_a_linked_list/linked_list.py
@@ -1,56 +1,57 @@
+# Singly Linked List Node
+class ListNode:
+    def __init__(self, val, next_node=None):
+        self.val = val
+        self.next = next_node
 
-class ListNode: 
-    def __innit__(self, val, next_node=None):
-        self.val = val 
-        self.next = next_node 
- 
-
-
+# Implementation for Singly Linked List
 class LinkedList:
-    
     def __init__(self):
-        #Dummy
+        # Init the list with a 'dummy' node which makes 
+        # removing a node from the beginning of list easier.
         self.head = ListNode(-1)
-        self.tail = self.head 
-
+        self.tail = self.head
     
     def get(self, index: int) -> int:
+        curr = self.head.next
+        i = 0
+        while curr:
+            if i == index:
+                return curr.val
+            i += 1
+            curr = curr.next
+        return -1  # Index out of bounds or list is empty
 
-        curr = self.head.head
-        i = 0 
-
-        while curr : 
-            if i == index: 
-                return curr.value 
-            curr= curr.next 
-            i += 1 
-
-        return -1 # index error 
-    
     def insertHead(self, val: int) -> None:
         new_node = ListNode(val)
-        new_node.next = self.head.next 
+        new_node.next = self.head.next
         self.head.next = new_node
-        # list was empty before inserting 
-        if not new_node.next: 
-            self.tail = new_node 
+        if not new_node.next:  # If list was empty before insertion
+            self.tail = new_node
 
     def insertTail(self, val: int) -> None:
         self.tail.next = ListNode(val)
-        self.tail = self.tail.next 
-
+        self.tail = self.tail.next
 
     def remove(self, index: int) -> bool:
+        i = 0
+        curr = self.head
+        while i < index and curr:
+            i += 1
+            curr = curr.next
         
+        # Remove the node ahead of curr
+        if curr and curr.next:
+            if curr.next == self.tail:
+                self.tail = curr
+            curr.next = curr.next.next
+            return True
+        return False
 
     def getValues(self) -> List[int]:
-
-        curr = self.head.next 
+        curr = self.head.next
         res = []
-        while curr: 
+        while curr:
             res.append(curr.val)
-            curr = curr.next 
-        
-        return res 
-        
-        
+            curr = curr.next
+        return res
