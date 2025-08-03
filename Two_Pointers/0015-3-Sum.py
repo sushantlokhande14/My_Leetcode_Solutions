2729759852630dea2f2d4a6f6222d75682c12c89
Sushant <sushantlokhande094@gmail.com>
2025-08-02 22:05:36 -0700

    added new directory and bruteforce solution for 3Sum problem

---
+++ Two_Pointers/0015-3-Sum.py
@@ -0,0 +1,16 @@
+# brute force solution , uses triple nested loops , time complexity O(n^3), space complexity O(1) 
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        n = len(nums)
+        nums.sort()
+
+        for i in range(n): 
+            for j in range(i+1,n): 
+                for k in range(j+1, n): 
+                    if nums[i] + nums[j]+ nums[k]== 0: 
+                        res.add(tuple([nums[i],nums[j],nums[k]]))
+
+        return [list(i) for i in res ] 
