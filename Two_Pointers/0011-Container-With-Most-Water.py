6147ced7e8734d5e924b37581a062682e81d1514
Sushant <sushantlokhande094@gmail.com>
2025-08-02 23:12:06 -0700

    added bruteforce solution for 0011

---
+++ Two_Pointers/0011-Container-With-Most-Water.py
@@ -0,0 +1,14 @@
+# Brute force solution, uses 2  nested loops, time complexity O(n^2), space complexity O(1)
+
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        
+        res = 0 
+        for i in range(len(heights)): 
+            for j in range(i+1 , len(heights)): 
+                area =( min(heights[i] , heights[j])* (j-i)) 
+
+                res = max(res, area)
+
+        return res 
