2ca24051bc0bb519cd38d277ac1806a0ff3b710a
Sushant <sushantlokhande094@gmail.com>
2025-08-05 15:57:39 -0700

    added structure

---
+++ Stacks/0020-Valid-Parantheses.py
@@ -0,0 +1,9 @@
+# LeetCode Problem: Valid Parentheses
+# https://leetcode.com/problems/valid-parentheses/ 
+
+# Brute Force solution for valid parentheses, time complexity O(n^2), space complexity O(1)
+
+
+
+# optimal solution using stack, time complexity O(n), space complexity O(n)
+
