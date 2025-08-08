a987fd6cf03d198ef87fb033f3f0bb408819ee36
Sushant <sushantlokhande094@gmail.com>
2025-08-07 21:47:37 -0700

    added new file for leetcode 150

---
+++ Stacks/0150-Evaluate-Reverse-Polish-Notation.py
@@ -0,0 +1,9 @@
+# Leetcode 0150. Evaluate Reverse Polish Notation
+# https://leetcode.com/problems/evaluate-reverse-polish-notation/   
+
+# This solution uses a stack to evaluate the expression in Reverse Polish Notation (RPN)
+# The stack is used to store numbers, and when an operator is encountered,  
+
+# the top two numbers are popped from the stack, the operation is performed,
+# and the result is pushed back onto the stack.                                       
+
