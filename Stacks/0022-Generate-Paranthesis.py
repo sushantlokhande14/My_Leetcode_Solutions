b7e097593d5e4c5d0b6e32a8c0716648d98d91ed
Sushant <sushantlokhande094@gmail.com>
2025-08-07 23:54:57 -0700

    added solution for generate parantheses

---
+++ Stacks/0022-Generate-Paranthesis.py
@@ -0,0 +1,25 @@
+# Leetcode 0022. Generate Parentheses
+# https://leetcode.com/problems/generate-parentheses/       
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
