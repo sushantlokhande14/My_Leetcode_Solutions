ce33295094d209200e73a674c00653a7f82f2170
Sushant <sushantlokhande094@gmail.com>
2025-08-07 21:27:08 -0700

    added outline to leetcode 155

---
+++ Stacks/0155-Minimum-Stack.py
@@ -0,0 +1,19 @@
+# Leetcode 0155. Minimum Stack
+# https://leetcode.com/problems/min-stack/    
+
+class MinStack:
+
+    def __init__(self):
+        
+
+    def push(self, val: int) -> None:
+        
+
+    def pop(self) -> None:
+        
+
+    def top(self) -> int:
+        
+
+    def getMin(self) -> int:
+        
