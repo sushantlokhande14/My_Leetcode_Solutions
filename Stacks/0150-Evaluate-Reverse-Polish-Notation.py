a12431b66fb9c99567ba2306c45e7194d3e4125d
Sushant <sushantlokhande094@gmail.com>
2025-08-07 22:17:58 -0700

    added the solution to evaluate polish notation.py

--- Stacks/0150-Evaluate-Reverse-Polish-Notation.py
+++ Stacks/0150-Evaluate-Reverse-Polish-Notation.py
@@ -7,3 +7,21 @@
 # the top two numbers are popped from the stack, the operation is performed,
 # and the result is pushed back onto the stack.                                       
 
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens: 
+            if c =="+":
+                stack.append(stack.pop() + stack.pop())
+            elif c =="*":
+                stack.append(stack.pop() * stack.pop())
+            elif c =="-":
+                a , b = stack.pop() , stack.pop()
+                stack.append(b-a) 
+            elif c =="/":
+                a , b = stack.pop() , stack.pop()
+                stack.append(int(b/a))
+            else: 
+                stack.append(int(c))
+
+        return stack[-1]
