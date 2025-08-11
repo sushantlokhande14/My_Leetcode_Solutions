7495391dcc4c959ce26fe77021c5b33bb4c17fb5
Sushant <sushantlokhande094@gmail.com>
2025-08-11 00:49:38 -0700

    added solution for car fleet problem

--- Stacks/0853-Car-Fleet.py
+++ Stacks/0853-Car-Fleet.py
@@ -1,3 +1,22 @@
 # Leetcode 0853. Car Fleet
 # https://leetcode.com/problems/car-fleet/
 
+# Optimized solution using stack
+# Time complexity O(nlogn), space complexity O(n)   
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p,s)for p,s in zip(position,speed) ]
+        pair.sort(reverse = True )
+        stack = []
+
+        for p,s in pair: 
+            time = (target-p)/s 
+            stack.append(time)
+            if len(stack) > 1 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+
+        return len(stack) 
+
+
