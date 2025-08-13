a56d80d2653d7f91885c6147390d4cedc46cc2b1
Sushant <sushantlokhande094@gmail.com>
2025-08-13 00:41:02 -0700

    added solution for return maximum subarray problem

--- Extras/Kadane's Algorithm/Kadanes-slidingwindow-returnarray.py
+++ Extras/Kadane's Algorithm/Kadanes-slidingwindow-returnarray.py
@@ -8,3 +8,24 @@ Sometimes, a problem may ask to return the actual subarray containing the larges
 # time Complexity: O(n)
 # space Complexity: O(1)
 
+# initializations 
+
+curSum = 0 
+maxSum = nums[0]
+maxL = 0 
+maxR = 0 
+
+
+for R in range(len(nums)): 
+    if curSum < 0 :
+        curSum = 0 
+        L = R 
+
+    curSum = nums[R]
+    
+    if curSum > maxSum : 
+        maxSum = curSum 
+        maxL = L 
+        maxR = R 
+
+return [maxL, maxR ]
