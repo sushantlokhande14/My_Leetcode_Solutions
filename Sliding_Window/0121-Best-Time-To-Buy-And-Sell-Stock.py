5e129ce47696db801533b2febb3276352dd049d1
Sushant <sushantlokhande094@gmail.com>
2025-08-16 23:27:15 -0700

    Added the bruteforce solution for leetcode 121

--- Sliding_Window/0121-Best-Time-To-Buy-And-Sell-Stock.py
+++ Sliding_Window/0121-Best-Time-To-Buy-And-Sell-Stock.py
@@ -2,3 +2,19 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/    
 
 
+# Brute Force Approach
+# Time Complexity: O(n^2) - nested loops to check all pairs
+# Space Complexity: O(1) - no extra space used
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProf = 0 
+        n = len(prices)
+
+        for i in range(n): 
+            for j in range(i+1 , n ): 
+                prof = prices[j] - prices[i]
+
+                maxProf = max(prof, maxProf)
+        
+        return maxProf
