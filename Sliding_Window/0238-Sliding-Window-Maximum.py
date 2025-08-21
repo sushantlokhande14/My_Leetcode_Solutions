f5d02c00986fc322ff6dd7bb5d69cea2c48a0f3e
Sushant <sushantlokhande094@gmail.com>
2025-08-21 02:51:51 -0700

    added optimal solution

--- Sliding_Window/0238-Sliding-Window-Maximum.py
+++ Sliding_Window/0238-Sliding-Window-Maximum.py
@@ -23,3 +23,35 @@ class Solution:
 
         
         return output 
+    
+
+# Optimized Approach using Deque
+# Time Complexity: O(n) - each element is added and removed from the deque at most
+# once
+# Space Complexity: O(k) - the deque can store at most k elements
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        l = 0 
+        r = 0 
+        q = collections.deque()
+        res = []
+
+        while r < len(nums): 
+
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+
+            q.append(r)
+
+            if l > q[0]: 
+                q.popleft()
+            
+            if r+1 >= k : 
+                res.append(nums[q[0]])
+                l+=1 
+            
+            r+= 1 
+
+        return res 
