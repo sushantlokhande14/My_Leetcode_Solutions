248cd350d90980173fe07754b8af00af8fb291e6
Sushant <sushantlokhande094@gmail.com>
2025-08-25 22:42:30 -0700

    added the recursive solution

--- Sliding_Window/Linked_Lists/0206-Reverse-A-Linked-List.py
+++ Sliding_Window/Linked_Lists/0206-Reverse-A-Linked-List.py
@@ -23,3 +23,28 @@ class Solution:
             curr = temp 
         
         return prev
+    
+
+# Recursive Approach - Time Complexity: O(n) - where n is the number of nodes in the linked list
+# Space Complexity: O(n) - the recursion stack can go up to n   
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Base case: if list is empty or only one node, return head
+        if not head or not head.next:
+            return head  
+
+        # Reverse the rest of the list
+        newHead = self.reverseList(head.next)
+
+        # Flip the pointer
+        head.next.next = head
+        head.next = None
+
+        return newHead 
