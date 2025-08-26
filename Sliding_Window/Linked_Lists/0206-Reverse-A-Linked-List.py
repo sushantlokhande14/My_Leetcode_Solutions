b94b5214b04eb61d87da212694e861263c3fe840
Sushant <sushantlokhande094@gmail.com>
2025-08-25 21:26:24 -0700

    added reverse linked list iterative solution

--- Sliding_Window/Linked_Lists/0206-Reverse-A-Linked-List.py
+++ Sliding_Window/Linked_Lists/0206-Reverse-A-Linked-List.py
@@ -1,4 +1,25 @@
 # Leetcode 0206. Reverse Linked List
 # https://leetcode.com/problems/reverse-linked-list/    
 
+# Iterative Approach    - Time Complexity: O(n) - where n is the number of nodes in the linked list
+# Space Complexity: O(1) - no extra space used
 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head 
+        prev = None 
+
+        while curr: 
+            temp = curr.next 
+            curr.next = prev 
+            prev = curr 
+            curr = temp 
+        
+        return prev
