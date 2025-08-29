75eada847b66cd411838b8559fa337043f6ca6f1
Sushant <sushantlokhande094@gmail.com>
2025-08-29 02:01:10 -0700

    added optimal solution for 141 using fast and slow pointers

--- Linked_Lists/0141-Linked-List-Cycle.py
+++ Linked_Lists/0141-Linked-List-Cycle.py
@@ -25,3 +25,31 @@ class Solution:
                 curr = curr.next 
 
         return False 
+    
+
+# solution 2: using two pointers (Floyd's Tortoise and Hare algorithm)
+# time complexity : O(n) where n is the number of nodes in the linked list
+# space complexity : O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head 
+        fast = head 
+
+        while fast and fast.next: 
+
+            slow = slow.next 
+            fast = fast.next.next
+
+
+            if slow == fast: 
+                return True 
+
+        
+        return False 
