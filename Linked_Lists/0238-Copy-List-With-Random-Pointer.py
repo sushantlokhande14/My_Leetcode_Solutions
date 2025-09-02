bebd1261210f0ecfb7166d00a0ae44f317312292
Sushant <sushantlokhande094@gmail.com>
2025-09-02 02:09:16 -0700

    added the soluion for neetcode 238

--- Linked_Lists/0238-Copy-List-With-Random-Pointer.py
+++ Linked_Lists/0238-Copy-List-With-Random-Pointer.py
@@ -1,3 +1,32 @@
 # leetcode problem 138
 # https://leetcode.com/problems/copy-list-with-random-pointer/  
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        mapp = {None : None}
+        curr = head 
+        while curr: 
+            copy = Node(curr.val) # makes a new node , with no links 
+            mapp[curr] = copy 
+            curr = curr.next 
+
+        
+        curr = head 
+
+        while curr:
+            copy = mapp[curr]
+            copy.next = mapp[curr.next] 
+            copy.random = mapp[curr.random] 
+            curr = curr.next
+
+        
+        return mapp[head]
