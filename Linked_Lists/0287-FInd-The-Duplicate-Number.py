6decb0b0eb2fe21306ebe8ae421595ba722555db
Sushant <sushantlokhande094@gmail.com>
2025-09-04 23:30:35 -0700

    added bruteforce solution for 287

--- Linked_Lists/0287-FInd-The-Duplicate-Number.py
+++ Linked_Lists/0287-FInd-The-Duplicate-Number.py
@@ -1,3 +1,19 @@
 #  Leetcode  287. Find the Duplicate Number
 #  https://leetcode.com/problems/find-the-duplicate-number/ 
-# 
+
+# brute force
+# time complexity O(NlogN) where N is total number of nodes in all the lists
+# space complexity O(N) for storing all the nodes in a list
+
+# the solution is NLOGN because we are sorting the list of nodes
+# and sorting takes NLOGN time  
+# and we are doing this for N nodes
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums)): 
+            if nums[i] == nums[i+1]: 
+                return nums[i]
+            
+
