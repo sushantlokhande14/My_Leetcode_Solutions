6389943f5a2955f45ad429a26d86b9e4b765b6fe
Sushant <sushantlokhande094@gmail.com>
2025-09-10 12:09:35 -0700

    added the brute force sol for leetcode 23

--- Linked_Lists/0023-Merge-K-Sorted-Link-Lists.py
+++ Linked_Lists/0023-Merge-K-Sorted-Link-Lists.py
@@ -1,3 +1,36 @@
 # leetcode 23 Merge k Sorted Lists
 # https://leetcode.com/problems/merge-k-sorted-lists/
 
+# brute force solution
+# time complexity O(N log N) where N is total number of nodes in all the lists
+# space complexity O(N) for storing all the nodes in a list
+
+# here we are using python's inbuilt sort function which uses Timsort algorithm
+# Timsort has a time complexity of O(N log N) in the average and worst
+# case, and O(N) in the best case (when the array is already sorted).
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        nodes = []
+        for head in lists: 
+            while head: 
+                nodes.append(head.val)
+                head = head.next 
+        
+        nodes.sort()
+
+        res = ListNode(0)
+        curr = res 
+        for node in nodes: 
+            curr.next = ListNode(node)
+            curr = curr.next 
+
+        return res.next 
+ 
