59cff7d7b9a130beda3bccc6d888c23e0e4fdd95
Sushant <sushantlokhande094@gmail.com>
2025-09-10 20:06:52 -0700

    added optimal solution for leetcode 25

--- Linked_Lists/0025-Reverse-Nodes-In-K-Group.py
+++ Linked_Lists/0025-Reverse-Nodes-In-K-Group.py
@@ -44,3 +44,42 @@ class Solution:
             prev = tail 
             curr = node 
   
+  # Optimal solution without using extra space
+# time complexity O(N) where N is number of nodes in the linked list
+# space complexity O(1)
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+
+        while True:
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+
+            prev, curr = kth.next, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next
+
+    def getKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
