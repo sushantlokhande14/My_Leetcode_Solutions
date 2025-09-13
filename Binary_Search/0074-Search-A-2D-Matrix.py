6fbc3f16d6db8d2ec2204dc37e1aff01989bd74e
Sushant <sushantlokhande094@gmail.com>
2025-09-12 22:41:31 -0700

    added the bruteforce solution for 0074

--- Binary_Search/0074-Search-A-2D-Matrix.py
+++ Binary_Search/0074-Search-A-2D-Matrix.py
@@ -7,3 +7,11 @@
 # Time Complexity: O(m*n)
 # Space Complexity: O(1)
 
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for r in range(len(matrix)): 
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == target: 
+                    return True 
+
+        return False 
