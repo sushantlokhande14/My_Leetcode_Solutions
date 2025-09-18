22e7955b582b1a8445bffb21298cc0e1ee6a8697
Sushant <sushantlokhande094@gmail.com>
2025-09-17 23:41:32 -0700

    added bruteforce for 875

--- Binary_Search/0875-Koko-Eating-Bananas.py
+++ Binary_Search/0875-Koko-Eating-Bananas.py
@@ -5,3 +5,17 @@
 # Time Complexity: O(n*max(piles))
 # Space Complexity: O(1)
 
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        speed = 1
+        while True: 
+            total_time = 0 
+            for pile in piles: 
+                total_time += math.ceil(pile/speed)
+
+            if total_time <= h: 
+                return speed 
+            
+            speed+= 1 
+
+        return speed
