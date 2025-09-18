bc773bf86afccdf582912e7954a9d63778bc8570
Sushant <sushantlokhande094@gmail.com>
2025-09-18 00:00:10 -0700

    added binary sol

--- Binary_Search/0875-Koko-Eating-Bananas.py
+++ Binary_Search/0875-Koko-Eating-Bananas.py
@@ -25,3 +25,21 @@ class Solution:
 # Time Complexity: O(n*log(max(piles)))
 # Space Complexity: O(1)
 
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
+
+        while l <= r:
+            k = (l + r) // 2
+
+            totalTime = 0
+            for p in piles:
+                totalTime += math.ceil(float(p) / k)
+            if totalTime <= h:
+                res = k
+                r = k - 1
+            else:
+                l = k + 1
+        return res
