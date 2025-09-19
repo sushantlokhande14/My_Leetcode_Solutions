8c63eefdda6d165ebc91cd75ccfeaf894e31c2f0
Sushant <sushantlokhande094@gmail.com>
2025-09-18 22:17:50 -0700

    added first brute force

--- Binary_Search/0153-Find-Minimum-In-Rotated-Sorted-Array.py
+++ Binary_Search/0153-Find-Minimum-In-Rotated-Sorted-Array.py
@@ -6,3 +6,10 @@
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 
+# This approach works because the array is guaranteed to be rotated, meaning there is at least one point where the order breaks. By iterating through the array and checking for this break, we can find the minimum element efficiently.
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        for i in range(len(nums)-1): 
+            if nums[i+1]< nums[i]:
+                return nums[i+1]
+        return nums[0]
