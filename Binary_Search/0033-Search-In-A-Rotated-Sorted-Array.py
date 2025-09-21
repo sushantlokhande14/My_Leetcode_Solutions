17a1d1a47d5292f5415b7d1d17fa9213a918cc0b
Sushant <sushantlokhande094@gmail.com>
2025-09-20 17:06:37 -0700

    added new directory for 33

---
+++ Binary_Search/0033-Search-In-A-Rotated-Sorted-Array.py
@@ -0,0 +1,7 @@
+# leetcode problem 33: `Search in a Rotated Sorted Array`
+# https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+# brute force
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
