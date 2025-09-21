9376075c14aefff654a398aaa2d365327ea40681
Sushant <sushantlokhande094@gmail.com>
2025-09-20 17:53:06 -0700

    added the optimal solution using Bin Search

--- Binary_Search/0033-Search-In-A-Rotated-Sorted-Array.py
+++ Binary_Search/0033-Search-In-A-Rotated-Sorted-Array.py
@@ -17,3 +17,32 @@ class Solution:
 #Binary Search
 # Time Complexity: O(log n)
 # Space Complexity: O(1)
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0 
+        r = len(nums)-1 
+         
+        while l<=r: 
+
+            m = (l+r)//2 
+            if target == nums[m]:
+                return m
+            
+            if nums[m] >= nums[l]:
+                if target > nums[m] or target < nums[l]:
+                    l = m + 1 
+                else: 
+                    r = m-1 
+                
+            else: 
+                if nums[m]> target or nums[r] < target:
+                    r = m - 1
+                else : 
+                    l = m+1 
+            
+        return -1 
+
+
+
