0307aaedafb872db01d52c9da893e6522eacada3
Sushant <sushantlokhande094@gmail.com>
2025-09-20 17:08:28 -0700

    added the brute force solution for 33

--- Binary_Search/0033-Search-In-A-Rotated-Sorted-Array.py
+++ Binary_Search/0033-Search-In-A-Rotated-Sorted-Array.py
@@ -5,6 +5,15 @@
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target: 
+                return i  
+        return -1 
+
+
 #Binary Search
 # Time Complexity: O(log n)
 # Space Complexity: O(1)
